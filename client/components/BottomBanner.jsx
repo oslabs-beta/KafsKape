fda73119a46f4b9704f14c03e694d0d3011200e7
miraal <miraalhaq@gmail.com>
2024-04-24 17:30:30 -0700

    Making BottomBanner items dynamic

--- client/components/BottomBanner.jsx
+++ client/components/BottomBanner.jsx
@@ -2,51 +2,48 @@ import React from 'react'
 import { Container, Grid, Box, Typography, Link } from '@material-ui/core'
 
 export default function BottomBanner() {
+  const gridItems = [
+    { title: "Need Help?", link: "https://github.com/oslabs-beta/Kafmira" },
+    { title: "About the Team", link: "http://kafmira.io/#clients-area" },
+    { title: "Contact Us", link: "http://kafmira.io/#support-area" },
+  ];
 
   return (
     <footer>
-    {/* can add sx prop to accept all system props as well as valid CSS */}
-    <Box  
-      p={2}
-      mt={2}
-      align='center'
-      sx={{
-        bgcolor: 'info.main',
-        color: 'white',
-        zIndex: 'tooltip',
-        left: '50%',
-        top: '100%'
-      }} 
-    >
-        <Container maxWidth='lg'> 
+      {/* can add sx prop to accept all system props as well as valid CSS */}
+      <Box
+        p={2}
+        mt={2}
+        align="center"
+        sx={{
+          bgcolor: "info.main",
+          color: "white",
+          zIndex: "tooltip",
+          left: "50%",
+          top: "100%",
+        }}
+      >
+        <Container maxWidth="lg">
           <Box m={1.5} borderBottom={1}>
-            <Typography
-              variant='h6'
-              color=''
-              gutterBottom
-            >
+            <Typography variant="h6" color="" gutterBottom>
               SSHK Technology Group
             </Typography>
           </Box>
-          <Grid container direction='row' spacing={6}>
-            <Grid item xs={12} sm={4}>
-              <Link onClick={()=> window.open('https://github.com/oslabs-beta/Kafmira', 'blank')} color='inherit'>
-                Need Help?
-              </Link>
-            </Grid>
-            <Grid item xs={12} sm={4}>
-              <Link onClick={()=> window.open('https://github.com/oslabs-beta/Kafmira', 'blank')} color='inherit'>
-                About the Team
-              </Link>
-            </Grid>
-            <Grid item xs={12} sm={4}>
-              <Link onClick={()=> window.open('https://github.com/oslabs-beta/Kafmira', 'blank')} color='inherit'>
-                Contact Us
-              </Link>
-            </Grid>
+          <Grid container direction="row" spacing={6}>
+            {gridItems.map((gridItem, i) => (
+              <Grid item key={i} xs={12} sm={4}>
+                <Link
+                  key={i}
+                  onClick={() => window.open(gridItem.link, "blank")}
+                  color="inherit"
+                >
+                  {gridItem.title}
+                </Link>
+              </Grid>
+            ))}
           </Grid>
         </Container>
       </Box>
     </footer>
-  )
+  );
 }
